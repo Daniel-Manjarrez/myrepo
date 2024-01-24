2a00b72975855d3a721806777ebb849ff9c308b8
Daniel Manjarrez <dam2274@columbia.edu>
2024-01-24 16:28:52 -0500

    Add an epic line

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    printf("Added from main branch\n");
     printf("Goodbye AP!\n");
     printf("Hello ASP! xD\n");
     printf("Added from dev branch\n");
+    printf("Branching is epic!\n")
 }
